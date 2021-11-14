c024df8458b3f415abdfffb341ac3c27e8fdd0de
Youssef Kbala <55252948+kirito01kun@users.noreply.github.com>
2021-11-14 23:08:30 +0100

    Update main.c

--- main.c
+++ main.c
@@ -1,6 +1,20 @@
 #include <stdio.h>
-#include "Exercice8.h"
+#include "Exercice1Serie1.h"
+#include "Exercice2Serie1.h"
+#include "Exercice3Serie1.h"
+#include "Exercice4Serie1.h"
+#include "Exercice5Serie1.h"
+#include "Exercice6Serie1.h"
+#include "Exercice7Serie1.h"
+#include "Exercice8Serie1.h"
 
 void	main(){
+	Ex1();
+	Ex2();
+	Ex3();
+	Ex4();
+	Ex5();
+	Ex6();
+	Ex7();	
 	Ex8();
 }
